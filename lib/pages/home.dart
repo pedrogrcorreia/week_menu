395dd2fe7560524a2a61ad0637832209d22924f3
Pedro Correia <pedrogrcorreia@gmail.com>
2023-01-06 01:54:48 +0000

    get from shared preferences before server

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -26,10 +26,52 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
   late SharedPreferences prefs;
-  List<WeekDay> weekDays = [];
+
+  Future<List<WeekDay>>? _weekDays;
+
+  Future<List<WeekDay>> _getWeekDays() async {
+    if (_weekDays == null) {
+      _weekDays = _fetchWeekDaysShared();
+    } else {
+      _weekDays = _fetchWeekDays();
+    }
+    return _weekDays!;
+  }
+
+  Future<List<WeekDay>> _fetchWeekDaysShared() async {
+    print("getting this list from shared preferences!");
+    List<WeekDay> weekDays = [];
+    prefs = await SharedPreferences.getInstance();
+    var weekDaysJson = prefs.getString("weekDays");
+    var data = json.decode(utf8.decode(weekDaysJson!.codeUnits));
+
+    var rest = data as LinkedHashMap<String, dynamic>;
+
+    rest.forEach((key, value) {
+      weekDays.add(WeekDay.fromJson(value));
+    });
+
+    var today = DateFormat('EEEE').format(DateTime.now());
+    var todayIndex = 0;
+
+    for (var i = 0; i < weekDays.length; i++) {
+      if (weekDays[i].weekDay == today.toUpperCase()) {
+        todayIndex = i;
+      }
+    }
+
+    var removedDays = weekDays.getRange(0, todayIndex);
+
+    weekDays.addAll(removedDays);
+
+    weekDays.removeRange(0, todayIndex);
+
+    return weekDays;
+  }
 
   Future<List<WeekDay>> _fetchWeekDays() async {
-    prefs = await _prefs;
+    print("getting this list from the server!");
+    List<WeekDay> weekDays = [];
     var response = await http.get(
       Uri.parse('http://192.168.1.86:8080/menu'),
     );
@@ -61,8 +103,7 @@ class _HomePageState extends State<HomePage> {
       weekDaysJson.add(element.toJson().toString());
     });
 
-    prefs.setStringList('weekDays', weekDaysJson);
-    print(prefs.getStringList('weekDays'));
+    prefs.setString('weekDays', response.body);
 
     return weekDays;
   }
@@ -70,6 +111,12 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
+    sharedWeekDays();
+    _weekDays = _fetchWeekDaysShared();
+  }
+
+  void sharedWeekDays() async {
+    prefs = await SharedPreferences.getInstance();
   }
 
   @override
@@ -93,7 +140,7 @@ class _HomePageState extends State<HomePage> {
                 height: MediaQuery.of(context).size.height,
                 child: Center(
                   child: FutureBuilder<List<WeekDay>>(
-                    future: _fetchWeekDays(),
+                    future: _weekDays,
                     builder: (BuildContext context,
                         AsyncSnapshot<List<WeekDay>> snapshot) {
                       if (snapshot.hasData) {
@@ -205,7 +252,8 @@ class _HomePageState extends State<HomePage> {
                             });
                       } else if (snapshot.hasError) {
                         print("Snapshot error ${snapshot.error}");
-                        return const Text('Oops, something happened');
+                        return const Text(
+                            'Não há dados armazenados para mostrar, atualize os dados pelo servidor!');
                       } else {
                         return const CircularProgressIndicator();
                       }
@@ -219,7 +267,9 @@ class _HomePageState extends State<HomePage> {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          setState(() {});
+          setState(() {
+            _weekDays = _fetchWeekDays();
+          });
         },
         backgroundColor: Colors.green,
         child: const Icon(Icons.refresh),
