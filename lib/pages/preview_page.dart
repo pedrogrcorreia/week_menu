e1a116256f27a6058120834e1f73e7002b5ad7ad
Pedro Correia <pedrogrcorreia@gmail.com>
2023-01-06 15:30:49 +0000

    translate title of preview page

--- lib/pages/preview_page.dart
+++ lib/pages/preview_page.dart
@@ -2,6 +2,8 @@ import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'dart:io';
 
+import 'package:week_menu/utils/strings.dart';
+
 class PreviewPage extends StatelessWidget {
   const PreviewPage({Key? key, required this.picture, required this.title})
       : super(key: key);
@@ -12,7 +14,7 @@ class PreviewPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text(title)),
+      appBar: AppBar(title: Text("Ementa de " + Strings.getWeekDay(title))),
       body: Center(
         child: Column(mainAxisSize: MainAxisSize.min, children: [
           Hero(
